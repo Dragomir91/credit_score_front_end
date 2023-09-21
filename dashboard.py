9dae9fb9b9f038200d32271e3e15ab7612ad26e0
Dragomir91 <49881674+Dragomir91@users.noreply.github.com>
2023-09-21 19:08:32 +0200

    Update dashboard.py
    test sans lib turtle

--- dashboard.py
+++ dashboard.py
@@ -1,11 +1,11 @@
-from turtle import color
+#from turtle import color
 import pandas as pd
 import streamlit as st
 import requests
 from streamlit_shap import st_shap
 import shap
 import numpy as np
-#import plotly.express as px
+import plotly.express as px
 
 def liste_id(model_uri, data):
     headers = {"Content-Type": "application/json"}
