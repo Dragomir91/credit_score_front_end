600bde94e3feb9cd3fcfef49b6ea6c0c8e5ce3da
Dragomir <dragomir91@hotmail.fr>
2023-09-21 18:33:31 +0200

    updates dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,7 +1,24 @@
+from turtle import color
 import pandas as pd
 import streamlit as st
 import requests
+from streamlit_shap import st_shap
+import shap
+import numpy as np
+import plotly.express as px
 
+def liste_id(model_uri, data):
+    headers = {"Content-Type": "application/json"}
+
+    data_json = {}
+    response = requests.request(
+        method='GET', headers=headers, url=model_uri, json=data_json)
+
+    if response.status_code != 200:
+        raise Exception(
+            "Request failed with status {}, {}".format(response.status_code, response.text))
+
+    return response.json()
 
 def request_prediction(model_uri, data):
     
@@ -39,10 +56,9 @@ def request_prediction_shap(model_uri, data3):
     
     headers = {"Content-Type": "application/json"}
     
-
-    data_json = {
-                 'shap_values':0
-                 }
+    data_json = {'id':data3[0],
+                 'decision_id': data3[1],
+                 'proba':data3[2]}
     response = requests.request(
         method='GET', headers=headers, url=model_uri, json=data_json)
 
@@ -70,80 +86,140 @@ def request_cout_metier(model_uri, data4):
 
 def main():
     API_URI = 'https://credit-score-backend.onrender.com/predict'
-    API_URI2 = 'https://credit-score-backend.onrender.com/predict/shap_id'
+    API_URI2 = 'https://credit-score-backend.onrender.com/predict/feature_id'
     API_URI3 = 'https://credit-score-backend.onrender.com/predict/explain'
-    API_URI4 = 'https://credit-score-backend.onrender.com/predict/cout_metier'
-    #API_URI = 'http://127.0.0.1:8000/predict'
-    #API_URI2 = 'http://127.0.0.1:8000/predict/shap_id'
-    #API_URI3 = 'http://127.0.0.1:8000/predict/explain'
-    #API_URI4 = 'http://127.0.0.1:8000/predict/cout_metier'
+    API_URI4 = 'https://credit-score-backend.onrender.com/predict/graph_id'
+    API_URI5 = 'https://credit-score-backend.onrender.com/id_client'
+
+
+ 
+    st.title('Information client')
+
+    data_client = liste_id(API_URI5, [0])
+        #st.write('liste des clients disponibles',data_client['id_client'])
+
+    df = {
+        "list_id": data_client["list_id"],
+        }
+
+    df2 = {
+        "info_id":data_client["infos_id"],
+    }
+
+
+    id = st.multiselect('choisir un id',df["list_id"])
+
+    #st.write('information client',pd.Series(data_client['information_client']))
 
     st.title('Scores clients')
     st.subheader('Decison credit')
 
-    id = st.number_input('saisir id client', value=100002., step=1.)
+    #id = st.number_input('saisir id client', value=id, step=1.)
+    try :
+        predict_btn = st.checkbox('Predire')
+        if predict_btn:
+            data = [id[0], 0,0]
+            pred = None
 
-    predict_btn = st.button('Predire')
-    if predict_btn:
-        data = [id, 0,0]
-        pred = None
+            pred = request_prediction(API_URI, data)
 
-        pred = request_prediction(API_URI, data)
-
-        df = {
-        "id_client":[pred["id"]],
-        "decision":[pred["decision_id"]],
-        "proba":[round(pred["proba"],2)]
-        }
-
-        st.write('liste des clients disponibles',pd.DataFrame(df))
-         
-        #st.write('y_pred {}'.format(pred))
-#######################################################################""""
-
-
-    shap_btn = st.button('informations sur cet id')
-    if shap_btn:
-        data2 = [id, 0,0]
-        data_shap = request_explain_shap(API_URI2, data2) 
-        df_json = pd.read_json(data_shap)
-        
-        st.write(pd.DataFrame(df_json.values,columns=df_json.columns, index = [str(id)]))
+            df = {
+            "id_client":[pred["id"]],
+            "decision":[pred["decision_id"]],
+            "proba":[round(pred["proba"],2)]
+            }
             
-                 
-##############################################SHAP_VALUE###################################################
-    shap_btn = st.button('shap_value')
-    with st.form("my_form"):
+            st.write('liste des clients disponibles',pd.DataFrame(df))
+            if df['decision'][0] == 0:
+                st.subheader('le crédit peut être accordé')
+
+            else :
+                st.subheader('le crédit est refusé')
+            
+            #st.write('y_pred {}'.format(pred))
+    #######################################################################""""
+
+
+        info_btn = st.checkbox('informations sur cet id')
+        if info_btn:
+            data2 = [id[0], 0,0]
+            data_shap = request_explain_shap(API_URI2, data2) 
+            df_json = pd.read_json(data_shap)
+            
+            st.write(pd.DataFrame(df_json.values,columns=df_json.columns, index = [id]))
+                
+                    
+    ##############################################SHAP_VALUE###################################################
+        shap_btn = st.checkbox('shap_value')
+       
         if shap_btn:
-            data3 = [[id],[0],[0]]
-            data_shap = request_prediction_shap(API_URI3, data3)        
-            try :
-                df = pd.DataFrame(data_shap)
-                st.write('df index',df.loc[df.ID == id,:].iloc[:,1:])
-                df2 = df.loc[df.ID == id,:].iloc[:,1:]
-                st.bar_chart(df2.iloc[0,:])
-                #st.write(data_shap['shap_values'])  
-                #st.write('liste disponibles',pd.DataFrame([data_shap['shap_values']],index=data_shap['id'], columns=data_shap['top_features']))
-                submitted = st.form_submit_button("Submit")
-  
-
-            except:
-                st.write('error')
-##############################################COUT_METIER#####################################################
+                
+                data3 = [id[0],0,0]
+                data_shap = request_prediction_shap(API_URI3, data3)                            
+                shap_values = shap.Explanation(values=np.array(data_shap["values"]),
+                base_values=data_shap["base_values"],
+                data = data_shap["data"],
+                feature_names=data_shap["feature_names"])
+                st_shap(shap.plots.waterfall(shap_values,max_display=10),height=500, width=1100)
 
 
 
-    shap_btn = st.button('coût du portefeuille clients')
-    if shap_btn:
-        data4 = [id, 0,0]
-        data = request_explain_shap(API_URI4, data4) 
-        
-        df =  {"gain" : [data["gain"] ],
-              "perte" : [data["perte"]],
-              "roc_auc_0" : [data["roc_auc_0"]],
-              "roc_auc_1" : [data["roc_auc_1"]],
-              "threshold" : [data["threshold"]]}
-        st.write(pd.DataFrame(df))
+
+                btn = st.button('visualition des données clients')
+                if btn:
+                                
+                        data4 = [id[0], 0,0]
+                        data = request_explain_shap(API_URI4, data4) 
+                        df = pd.read_json(data)
+                        
+                        EXT_SOURCE2 = df.loc[df.SK_ID_CURR == id[0],"EXT_SOURCE_2"]
+                        EXT_SOURCE2_MEAN = df.loc[:,"EXT_SOURCE_2"].mean()
+                        AMT_ANNUITY = df.loc[df.SK_ID_CURR == id[0],"AMT_ANNUITY"]
+                        AMT_ANNUITY_MEAN = df.loc[:,"AMT_ANNUITY"].mean()
+                        EXT_SOURCE2_ACCEPT = df.loc[df['Y_PRED_PV_ID'] == 0,'EXT_SOURCE_2_PV_ID'].mean()
+                        EXT_SOURCE2_REFUSE = df.loc[df['Y_PRED_PV_ID'] == 1,'EXT_SOURCE_2_PV_ID'].mean()
+                        AMT_ANNUITY_ACCEPT = df.loc[df['Y_PRED_PV_ID'] == 0,'AMT_ANNUITY_PV_ID'].mean()
+                        AMT_ANNUITY_REFUSE = df.loc[df['Y_PRED_PV_ID'] == 1,'AMT_ANNUITY_PV_ID'].mean()
+
+                        chart_data = pd.DataFrame({
+                                        'ID' : ['ID_EXT_SOURCE_2','EXT_SOURCE2_MEAN','EXT_SOURCE2_ACCEPT','EXT_SOURCE2_REFUSE'],
+                                        'RESULTAT' : [EXT_SOURCE2.values[0], EXT_SOURCE2_MEAN,EXT_SOURCE2_ACCEPT,EXT_SOURCE2_REFUSE],
+                                        'COLOR' : [2,3,4,5]})
+                        
+                        chart_data2 = pd.DataFrame({
+                                        'ID' : ['ID_AMT_ANNUITY','AMT_ANNUITY','AMT_ANNUITY_ACCEPT','AMT_ANNUITY_REFUSE'],
+                                        'RESULTAT' : [ AMT_ANNUITY.values[0], AMT_ANNUITY_MEAN,AMT_ANNUITY_ACCEPT,AMT_ANNUITY_REFUSE],
+                                        'COLOR' : [2,3,4,5]})
+                        
+
+                        fig = px.bar(
+                                chart_data,
+                                x="ID",
+                                y="RESULTAT",
+                                color='ID',
+                                hover_name="ID",
+                                )
+                        
+                        fig2 = px.bar(
+                                chart_data2,
+                                x="ID",
+                                y="RESULTAT",
+                                color = 'ID',
+                                hover_name="ID")                       
+                        tab1, tab2 = st.tabs(["EXT_SOURCE2", "AMT_ANNUITY"])
+                        with tab1:
+                            # Use the Streamlit theme.
+                            # This is the default. So you can also omit the theme argument.
+                            st.plotly_chart(fig)
+                        with tab2:
+                            # Use the native Plotly theme.
+                            st.plotly_chart(fig2)
+
+                              
+    
+    ##############################################COUT_METIER#####################################################
+
+    except: 'un seul id doit être séléctionner'
 
 if __name__ == '__main__':
     main()
