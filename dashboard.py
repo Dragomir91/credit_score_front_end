f774764ea68482b9c05e0e03426782f680bb9f2b
Dragomir91 <49881674+Dragomir91@users.noreply.github.com>
2023-09-22 19:45:00 +0200

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -154,12 +154,13 @@ def main():
         if shap_btn:
                 
                 data3 = [id[0],0,0]
-                data_shap = request_prediction_shap(API_URI3, data3)                            
+                data_shap = request_prediction_shap(API_URI3, data3) 
+                st.write(data_shap)
                 shap_values = shap.Explanation(values=str(data_shap["values"]),
                 base_values=data_shap["base_values"],
                 data = str(data_shap["data"]),
                 feature_names=data_shap["feature_names"])
-                #st.write(data_shap)
+                
                 #st_shap(shap.plots.waterfall(shap_values,max_display=10),height=500, width=1100)
 
 
